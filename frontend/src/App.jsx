23bc423ea83daeb869aed9e405e1a5eef65e57fc
prasath <prasathramakrishnan005@gmail.com>
2025-11-28 22:52:18 +0530

    updated the login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import Login from "./Auth/Login";   
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <h1>MainPage</h1>
-      </div>
+      <Login />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
